33fa81b316d753933f5d05008158630c8d57a0b4
Zzz212zzZ <2120020201@qq.com>
2023-06-22 12:14:50 +0800

    更新后端app.py文件

--- back-end/app.py
+++ back-end/app.py
@@ -1,3 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_restful import Api
+from flask_restful import Resource
 
 app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return {'message': 'Hello, World!'}
+
+
+
+if __name__ == '__main__':
+    app.run()

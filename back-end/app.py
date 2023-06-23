a7b707e977826a5e63d6cbf6ec863c1112fcd97d
Zzz212zzZ <2120020201@qq.com>
2023-06-23 10:35:40 +0800

    123

--- back-end/app.py
+++ back-end/app.py
@@ -2,9 +2,6 @@ from flask import Flask,request
 from flask_cors import CORS
 from flask_restful import Api
 from flask_restful import Resource
-import json
-
-from DatabaseConnector import DatabaseConnector
 
 app = Flask(__name__)
 CORS(app)
